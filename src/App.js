80046e6476f93676e10f855bf859bcac593959e3
Pranesh Monda <praneymonda@gmail.com>
2023-06-27 13:34:56 -0400

    getting ready for css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import Employee from './components/Employee'
+import { useState } from 'react';
 
 function App() {
+  const [role,setRole] = useState();
+  console.log('we are about to list the employees');
+  const showEmployees = true;
+  var result;
+  if(showEmployees) {
+      result = 
+      <>
+      <input type='text' onChange={
+                  (e) => { 
+                    setRole(e.target.value);
+        }}/>
+
+        <Employee name="Pranesh" role="Manager"/>
+        <Employee name="Bob" role={role}/>
+        <Employee />
+        <Employee />
+        <Employee />
+      </>;
+  }
+  else {
+    result = <p>Employees can't be viewed</p>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      {result}
   </div>
   );
 }
