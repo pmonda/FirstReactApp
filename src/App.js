ea019c4cb9f0db05f5e90db238741ed87b5f6227
Pranesh Monda <praneymonda@gmail.com>
2023-06-29 20:24:13 -0400

    intro pages and children props

--- src/App.js
+++ src/App.js
@@ -5,115 +5,17 @@ import { useState } from 'react';
 import React from 'react';
 import {v4 as uuidv4} from 'uuid';
 import AddEmployee from './components/AddEmployee';
+import Header from './components/Header';
+import Employees from './pages/Employees';
 // import uuidv4 as v4
 
 function App() {
-  // const [role,setRole] = useState();
-  const [employees,setEmployees] = useState(
-    [
-      { 
-        id: 1,
-        name:  "Pranesh", 
-        role: "Manager",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      { 
-        id: 2,
-        name:  "Bob", 
-        role: "Developer",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      { 
-        id: 3,
-        name:  "Jack", 
-        role: "Quality Control",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
+  return(
+  <Header>
+      <Employees/>
 
-      { 
-        id: 4,
-        name:  "Pranesh", 
-        role: "Manager",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      { 
-        id:5,
-        name:  "Bob", 
-        role: "Developer",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      { 
-        id:6,
-        name:  "Jack", 
-        role: "Quality Control",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-
-      { 
-        id:7,
-        name:  "Pranesh", 
-        role: "Manager",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      { 
-        id:8,
-        name:  "Bob", 
-        role: "Developer",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      },
-      { 
-        id:9,
-        name:  "Jack", 
-        role: "Quality Control",
-        img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      }
-    ]
-  );
-  console.log('we are about to list the employees');
-  function updateEmployee(id, newName, newRole) {
-    const updateEmployees = employees.map((employee) => {
-      if(id === employee.id) {
-        //return new
-        return {...employee, name:newName, role:newRole};
-      }
-      return employee;
-    });
-    setEmployees(updateEmployees);
-  }
-
-
-  function newEmployee(name, role, img) {
-    const newEmp = {
-        id: uuidv4(),
-        name:  name, 
-        role: role,
-        img: img
-    }
-    setEmployees([...employees, newEmp]);
-  }
-  return (
-    <div className="App">
-      
-      <div className="flex flex-wrap justify-center">
-        {
-          
-          employees.map((employee) => {
-            return (<Employee 
-              key={employee.id}
-              id={employee.id}
-              name={employee.name} 
-              role={employee.role} 
-              img={employee.img}
-              updateEmployee={updateEmployee}
-              />)
-          }) 
-        } 
-      </div>
-
-      <AddEmployee newEmployee={newEmployee}>
-            </AddEmployee>
-      
-  </div>
+  </Header>
   );
 }
+
 export default App;
---
+++ src/pages/Employees.js
@@ -0,0 +1,119 @@
+import '../App.css';
+import Employee from '../components/Employee';
+import { useState } from 'react';
+import React from 'react';
+import {v4 as uuidv4} from 'uuid';
+import AddEmployee from '../components/AddEmployee';
+import Header from '../components/Header';
+
+function Employees() {
+    // const [role,setRole] = useState();
+    const [employees,setEmployees] = useState(
+      [
+        { 
+          id: 1,
+          name:  "Pranesh", 
+          role: "Manager",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        { 
+          id: 2,
+          name:  "Bob", 
+          role: "Developer",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        { 
+          id: 3,
+          name:  "Jack", 
+          role: "Quality Control",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+  
+        { 
+          id: 4,
+          name:  "Pranesh", 
+          role: "Manager",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        { 
+          id:5,
+          name:  "Bob", 
+          role: "Developer",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        { 
+          id:6,
+          name:  "Jack", 
+          role: "Quality Control",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+  
+        { 
+          id:7,
+          name:  "Pranesh", 
+          role: "Manager",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        { 
+          id:8,
+          name:  "Bob", 
+          role: "Developer",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        },
+        { 
+          id:9,
+          name:  "Jack", 
+          role: "Quality Control",
+          img: "https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+      ]
+    );
+    console.log('we are about to list the employees');
+    function updateEmployee(id, newName, newRole) {
+      const updateEmployees = employees.map((employee) => {
+        if(id === employee.id) {
+          //return new
+          return {...employee, name:newName, role:newRole};
+        }
+        return employee;
+      });
+      setEmployees(updateEmployees);
+    }
+  
+  
+    function newEmployee(name, role, img) {
+      const newEmp = {
+          id: uuidv4(),
+          name:  name, 
+          role: role,
+          img: img
+      }
+      setEmployees([...employees, newEmp]);
+    }
+    return (
+      <div className="App bg-gray-300 min-h-screen">
+        
+        <div className="flex flex-wrap justify-center my-2">
+          {
+            
+            employees.map((employee) => {
+              return (<Employee 
+                key={employee.id}
+                id={employee.id}
+                name={employee.name} 
+                role={employee.role} 
+                img={employee.img}
+                updateEmployee={updateEmployee}
+                />)
+            }) 
+          } 
+        </div>
+  
+        <AddEmployee newEmployee={newEmployee}>
+              </AddEmployee>
+        
+    </div>
+    );
+  }
+  export default Employees;
+  
